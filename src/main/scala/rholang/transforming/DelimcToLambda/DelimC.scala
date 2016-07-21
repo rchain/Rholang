4055991420cdcae9ca7f007e32a44f402ccdac2d
Jack Pettersson <jackpettersson@gmail.com>
2016-07-21 16:30:23 +0200

    initial (faulty) implementation of the CC monad directly in Scala

--- src/main/scala/rholang/transforming/DelimcToLambda/DelimC.scala
+++ src/main/scala/rholang/transforming/DelimcToLambda/DelimC.scala
@@ -1,65 +1,59 @@
 package rholang.transforming.DelimcToLambda
 
 // import rholang.transforming.DelimcToLambda.TypedLambda._
-// import rholang.transforming.DelimcToLambda.Prompt._
-// import rholang.transforming.DelimcToLambda.Seq._
+import rholang.transforming.DelimcToLambda.Prompt._
+import rholang.transforming.DelimcToLambda.Seq._
 
-// import scalaz.Monad
+import scalaz.Monad
 
 /**
   * Created by weeeeeew on 2016-06-09.
   */
-/*
-object DelimC {
-  case class Cont[Ans, A, B](k: Expr[A => MC[Ans, B]])
 
-  def unCont[Ans, A, B](cont: Cont[Ans, A, B], a: A) : MC[Ans, B] = cont match {
+object DelimC {
+  case class Cont[Ans, A, B](k: A => MC[Ans, B])
+
+  def unCont[Ans, A, B](cont: Cont[Ans, A, B])(a: A) : MC[Ans, B] = cont match {
     case Cont(k) => k(a)
   }
 
-  type MetaCont[Ans, A] = Seq[Cont[_,_,_], Ans, A]
-  type SubCont[Ans, A, B] = SubSeq[Cont[_,_,_], Ans, A, B]
-  case class CC[Ans, A, B](c: Cont[Ans, A, B] => MC[Ans, B])
+  type MetaCont[Ans, A] = Seq[Cont, Ans, A]
+  type SubCont[Ans, A, B] = SubSeq[Cont, Ans, A, B]
 
-  def unCC[Ans, A, B](cc: CC[Ans, A, B], cont: Cont[Ans, A, B]) : MC[Ans, B] = cc match {
+  type CCFun[Ans, A, B] = Cont[Ans, A, B] => MC[Ans, B]
+  case class CC[Ans, A](c: CCFun[Ans, A, B] forSome {type B})
+
+  def unCC[Ans, A](cc: CC[Ans, A])(cont: Cont[Ans, A, B] forSome {type B}) : MC[Ans, B] forSome {type B} = cc match {
     case CC(c) => c(cont)
   }
 
-    trait CC[Ans, A] {
-    type X
-    val c: Cont[Ans, A, X] => MC[Ans, X]
-  }
-
-
-  type ContToMC[Ans, A, B] = Cont[Ans, A, B] => MC[Ans, B]
-
-  case class CC[Ans, A](c: ContToMC[Ans, A, B forSome { type B }])
-
-
-  //def unCC[Ans, A](cc: CC[Ans, A]) = cc.c
-
   case class MC[Ans, B](m: MetaCont[Ans, B] => P[Ans, Ans])
 
-  def unMC[Ans, B](mc: MC[Ans, B], metaCont: MetaCont[Ans, B]) : P[Ans, Ans] = mc match {
+  def unMC[Ans, B](mc: MC[Ans, B])(metaCont: MetaCont[Ans, B]) : P[Ans, Ans] = mc match {
     case MC(m) => m(metaCont)
   }
 
-  class CCMonad[Ans,C] extends Monad[({type l[A] = CC[Ans, A]})#l] {
-    override def point[A](e: => A): CC[Ans, A] =
-      new CC[Ans, A]{
-        type X=C
-        val c = (cont: Cont[Ans, A, C]) => unCont(c, e)
-      }
+  class CCMonad[Ans] extends Monad[Lambda[A => CC[Ans, A]]] {
+    override final def point[A](e: => A): CC[Ans, A] = CC(k => unCont(k)(e))
 
-    override def bind[A, B](cc: CC[Ans, A])(e2: A => CC[Ans, B]): CC[Ans, B] = {
-      val e1 = cc.c
-      new CC[Ans, B] {
-        type X = cc.X
-
-        val c: Cont[Ans, B, cc.X] => MC[Ans, cc.X] = k => e1[Ans,A,cc.X]( Cont[Ans,A,cc.X]((v1: A) => e2(v1).c(???)))
-      }
-    }
+    override final def bind[A, B](e1: CC[Ans, A])(e2: (A) => CC[Ans, B]): CC[Ans, B] =
+      CC(k => unCC(e1)(Cont(v1 => unCC(e2(v1))(k))))
   }
-}
 
-*/
+  def appseg[Ans, A, B](k: Cont[Ans, A, B], a: A): MC[Ans, B] = unCont(k)(a)
+
+  def runOne[Ans, A](c: CC[Ans, A]): MC[Ans, A] = {
+    val initk = Cont(a => MC(mk => appmk(mk, a)))
+    unCC(c)(initk)
+  }
+
+  def appmk[Ans, A](mk: MetaCont[Ans, A], a: A): P[Ans, Ans] = mk match {
+    case EmptyS()        => ???
+    case PushP(_, mk2)   => appmk(mk2, a)
+    case PushSeg(k, mk2) => unMC(appseg(k, a))(mk2)
+  }
+
+  def runTwo[Ans](c: MC[Ans, Ans]): P[Ans, Ans] = unMC(c)(EmptyS())
+
+  def runCC[A](ce: CC[Ans, A] forSome {type Ans}): A = runP(runTwo(runOne(ce)))
+}
--- src/main/scala/rholang/transforming/DelimcToLambda/Prompt.scala
+++ src/main/scala/rholang/transforming/DelimcToLambda/Prompt.scala
@@ -1,37 +1,45 @@
 package rholang.transforming.DelimcToLambda
 
 import scalaz.Monad
-import rholang.transforming.DelimcToLambda.TypedLambda._
 
 /**
   * Created by weeeeeew on 2016-06-07.
   */
 object Prompt {
-  case class Prompt[Ans,A](p : ETyped[Integer])
+  case class Prompt[Ans,A](p : Int)
 
-  case class P[Ans,A](f : ETyped[Integer => (Integer, A)])
-
-  def unP[Ans,A](p: P[Ans,A], i: ETyped[Integer]) : ETyped[(Integer,A)] = p match {
-    case P(f) => applyAbs(f,i)
+  def unPrompt[Ans,A](prompt: Prompt[Ans,A]): Int = prompt match {
+    case Prompt(p) => p
   }
 
-  class MonadP[Ans] extends Monad[P[Ans,_]] {
-    override def point[A](e: => A): P[Ans, A] = P[Ans, A]((s: Integer) => (s,e))
+  case class P[Ans,A](f : Int => (Int, A))
 
-    override def bind[A, B](pa: P[Ans,A])(e2: A => P[Ans,B]): P[Ans,B] =
-      P[Ans,B]((s1: Integer) => unP[Ans,A](pa, s1) match {
-        case (s2,v1) => unP[Ans,B](e2(v1), s2)
+  def unP[Ans,A](p: P[Ans,A])(i: Int): (Int,A) = p match {
+    case P(f) => f(i)
+  }
+
+  class PMonad[Ans] extends Monad[({type Lambda[A] = P[Ans, A]})#Lambda] {
+    override final def point[A](e: => A): P[Ans,A] = P[Ans,A]((s: Int) => (s,e))
+
+    override final def bind[A, B](pa: P[Ans,A])(e2: A => P[Ans,B]): P[Ans,B] =
+      P((s1: Int) => unP(pa)(s1) match {
+        case (s2,v1) => unP(e2(v1))(s2)
       })
   }
 
-  def runP[Ans](pe: P[Ans,Ans]) = snd2(unP(pe, typedInt(0)))
-/*
-  def newPromptName[Ans, A] : P[Ans, Prompt[Ans, A]] = for{np <- newName}
-    yield newAbs(np, TSimple[Integer], tuple2(add(newVar(np), typedInt(1)), Prompt(newVar(np)))
+  def runP[Ans](pe: P[Ans,Ans]): Ans = unP(pe)(0)._2
 
-  def eqPrompt[Ans, A, B](pa : Prompt[Ans, A], pb : Prompt[Ans, B]) : Equal[A,B] = (pa, pb) match {
-    case (Prompt(p1), Prompt(p2)) if p1 == p2 => IsEqual(pa,pb)
-    case (Prompt(p1), Prompt(p2))             => IsNotEqual(pa,pb)
-  }
-*/
+  def newPromptName[Ans,A]: P[Ans, Prompt[Ans,A]] = P((np: Int) => (np+1, Prompt(np)))
+
+  sealed trait Equal[A,B]
+  case class IsEqual[A]() extends Equal[A,A]
+  case class IsNotEqual[A,B]() extends Equal[A,B]
+
+  def eqPrompt[Ans,A,B](p1: Prompt[Ans,A], p2: Prompt[Ans,B]): Equal[A,B] =
+    if (unPrompt(p1) == unPrompt(p2)) {
+      IsEqual().asInstanceOf[Equal[A,B]]
+    } else {
+      IsNotEqual()
+    }
+
 }
