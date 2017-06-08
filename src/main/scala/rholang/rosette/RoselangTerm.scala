cbe6433edd9f54bc98268d13d3649fdefe6c71b9
Kent Shikama <kent@kentshikama.com>
2017-06-07 21:51:28 -0700

    Remove unused serializeLetExpr

--- src/main/scala/rholang/rosette/RoselangTerm.scala
+++ src/main/scala/rholang/rosette/RoselangTerm.scala
@@ -81,13 +81,6 @@ case class StrTermPtdCtxtBr(override val nameSpace: String,
         case _ :: _ => {
           var acc = ""
 
-          def serializeLetExpr(lbl: TermCtxt[String, Either[String, String], String] with Factual with RosetteSerialization[String, Either[String, String], String], i: Int) = {
-            if (i == labels.length - 1) {
-              acc = "[" + acc + " ] " + lbl.rosetteSerialize
-            } else {
-              acc = acc + " " + lbl.rosetteSerialize.map({ case '(' => '[' case ')' => ']' case c => c })
-            }
-          }
 
           def serializeExpr(lbl: TermCtxt[String, Either[String, String], String] with Factual with RosetteSerialization[String, Either[String, String], String], i: Int) = {
             if (i == 0) {
